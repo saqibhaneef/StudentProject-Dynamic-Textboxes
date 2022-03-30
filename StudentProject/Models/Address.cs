e2d991b39d9cfa4ac6d8b25d5cd1f16df03a598d
Saqib <saqib.habif.saufik@outlook.com>
2022-03-30 14:01:16 +0500

    Revert "Revert "Add project files.""
    This reverts commit 866c32b650a611c54bcb3b9b8a4ad884751dfbe7.

---
+++ StudentProject/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentProject.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int StudentId { get; set; }
+        public Student student { get; set; }
+    }
+}
---
+++ StudentProject/Models/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentProject.Models
+{
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Class { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string addresses2 { get; set; }
+        public List<Address> addresses { get; set; }
+        public List<Student> students { get; set; }
+    }
+}
